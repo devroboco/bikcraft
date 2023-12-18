f48cbe02a66787a08ef3067531d3c3eae48e5664
Victor Dias Noleto <lzero1541@gmail.com>
2023-12-18 21:23:54 -0200

    javascript perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -17,8 +17,6 @@ links.forEach(ativarLink)
 
 const parametros = new URLSearchParams(location.search)
 
-console.log(parametros)
-
 function ativarProduto(parametro) {
     const elemento = document.getElementById(parametro)
     if (elemento) {
@@ -27,3 +25,23 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button")
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls')
+    const resposta = document.getElementById(controls)
+
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains('ativa');
+    pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas)
